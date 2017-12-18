1d04aff55f793bb57b2cf6c1247dd161b85f9773
Martin Leung <martinycleung@gmail.com>
2017-12-18 15:13:50 +0800

    Update runUnittestShell.sh

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -10,5 +10,5 @@ find . -name "*Test.py" -print | while read f; do
         ###
 done
 
-cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
-cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
+cp -r ./coverage.xml /var/lib/jenkins/workspace/python123/coverage.xml
+cp -r ./python_unittests_xml /var/lib/jenkins/workspace/python123/python_unittests_xml
